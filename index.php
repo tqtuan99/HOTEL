7427397ff6b6a21ae56384b55d5e7f657f9bf613
Trần Quốc Tuấn <quoctuancr1010@gmail.com>
2021-12-02 23:08:38 +0700

    structure, header-footer in home

--- index.php
+++ index.php
@@ -7,16 +7,20 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <link rel="stylesheet" href="./output.css">
    <link rel="stylesheet" href="./assets/css/styles.css">
+   <link rel="stylesheet" href="./assets/components/home/footer/footer.css">
    <link rel="stylesheet" href="./assets/font/fontawesome-free-5.15.4-web/css/all.min.css">
    <title>Document</title>
 </head>
-
 <body>
-  <div class="app">
-     <div class="header">
-        <a href="./assets/components/login/login.html">Login</a>
-     </div>
-  </div>
+   <?php
+   // *Header 
+   include './assets/components/home/header/header.html';
+
+   // *Body
+   
+   // *Footer
+   include './assets/components/home/footer/footer.html';
+   ?>
 </body>
 
 </html>
