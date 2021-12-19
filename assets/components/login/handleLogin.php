379a05257ae9a0a85a552a777dcba9c71aa0451a
long40129 <nguyendailong2k@gmail.com>
2021-12-19 17:35:41 +0700

    update tùm lum :v

--- assets/components/login/handleLogin.php
+++ assets/components/login/handleLogin.php
@@ -1,11 +1,16 @@
 <?php
 require_once("../handle/configDB.php");
+// $cookie_time = 30 * 24 * 60 * 60;
 
 
 if (isset($_POST["submit"])) {
    $email = $_POST["email"];
-   $pass = $_POST["password"];
+   // if(isset($_COOKIE['password']))
+   //    $pass = $_COOKIE['password'];
+   // else
+      $pass = md5($_POST["password"]);
 
+   $check_remember = ((isset($_POST['chk-remember']) != 0) ? 1 : "");
 
    $sql = "SELECT * FROM taikhoan";
    $result = $conn->query($sql);
@@ -14,25 +19,38 @@ if (isset($_POST["submit"])) {
    if ($result->num_rows > 0) {
       // output data of each row
       while ($row = $result->fetch_assoc()) {
-         if ($row["tendangnhap"] == $email){
-            if ($row['matkhau'] == $pass){
-               if ($row['trangthai'] ==1 ) {
+         if ($row["tendangnhap"] == $email) {
+            if ($row['matkhau']== $pass) {
+               if ($row['trangthai'] == 1) {
+                  if ($check_remember) {
+                     if(!isset($_COOKIE['password'])&& !isset($_COOKIE['password'])){
+                        setcookie('password', md5($row["matkhau"]), time() + $cookie_time);
+                        setcookie('username', $row["tendangnhap"], time() + $cookie_time);
+                     }
+                  }else {
+                     if(isset($_COOKIE['password'])){
+                        setcookie('password', '');
+                        setcookie('username', '');
+                     }
+                  }
                   $_SESSION['idUser'] = $row['idtaikhoan'];
                   header("Location: ../../../index.php");
                }
-               $check =1;
+               $check = 1;
             }
-            $check =2;
+            $check = 2;
          }
       }
       if ($check == 2) {
          echo '<div style="color: red;">Tên đăng nhập hoặc mật khẩu không chính xác.</div>';
-      }else
+      } else
       if ($check == 0) {
          echo '<div style="color: red;">Tài khoản không tồn tại. Vui lòng tạo tài khoản.</div>';
       } else {
          echo '<div style="color: red;">Tài khoản chưa xác nhận!</div>';
       }
+   }else{
+      echo '<div style="color: red;">Tài khoản không tồn tại. Vui lòng tạo tài khoản.</div>';
    }
 }
 $conn->close();
