1bcaf3e5f596c1988dece6e90e7237386a7557aa
Trần Quốc Tuấn <quoctuancr1010@gmail.com>
2021-12-20 00:19:12 +0700

    update header, formChangePass, formForgot

--- assets/components/forgotPass/forgotPass.php
+++ assets/components/forgotPass/forgotPass.php
@@ -1,42 +1,55 @@
-
 <?php
 require_once("../handle/dbcontroller.php");
 include('../handle/configDB.php');
-$check=0;
+$check = 0;
 if (isset($_POST["submit"])) {
     $email = $_POST["email"];
 
     $newPass = random_int(10000000, 99999999);
 
-        
-        $query = "SELECT * FROM khachhang";
-        $result = $conn->query($query);
 
-        while($row=$result->fetch_assoc())
-            if ($row['email'] == $email) {
-                $querry = "UPDATE taikhoan SET matkhau = $newPass";
-                $conn->query($querry);
-                include("../../../gmail-email/sendmail.php");
-                sendmail($email,'',$newPass,0);
-                $check=1;
-            }
+    $query = "SELECT * FROM khachhang";
+    $result = $conn->query($query);
+
+    while ($row = $result->fetch_assoc())
+        if ($row['email'] == $email) {
+            $querry = "UPDATE taikhoan SET matkhau = $newPass";
+            $conn->query($querry);
+            include("../../../gmail-email/sendmail.php");
+            sendmail($email, '', $newPass, 0);
+            $check = 1;
+        }
     $conn->close();
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../register/confirmRegister.css">
+
     <title>Forgot Password</title>
 </head>
+
 <body>
-    <form action="" method="POST">
-        <label for="">Nhập email của bạn</label>
-        <input type="text" name="email">
-        <input type="submit" name="submit">
-        <div><?php if (isset($_POST["submit"])) if ($check == 0) echo "Email không tồn tại. <br><a href='../register/register.php?email=$email'>Quay lại trang đăng kí</a>"; else echo "Chúng tôi đã gửi mật khẩu mới vào email: $email <br><a href='../login/login.php?email=$email'>Quay lại trang đăng nhập</a>"; ?></div>
+    <div class="cont-confirm">
+        <form action="" method="POST">
+            <div class="bg-confirm">
+                <div class="confirm-text" style="margin-top: 50px;">
+                    <label for="" class="lbl-pass-center" style="font-size: 25px;">Nhập email của bạn</label>
+                    <input type="text" class="input pass confirmCode" name="email" placeholder="user@gmail.com" required>
+                    <button type="submit" style="margin-top: 50px;" class="btn signin btn-confirmCode" name="submit">SEND</button>
+                    <div>
+                        <?php if (isset($_POST["submit"])) if ($check == 0) echo "Email không tồn tại. <br><a href='../register/register.php?email=$email'>Quay lại trang đăng kí</a>";
+                        else echo "Chúng tôi đã gửi mật khẩu mới vào email: $email <br><a href='../login/login.php?email=$email'>Quay lại trang đăng nhập</a>"; ?>
+                    </div>
+                </div>
+            </div>
+    </div>
     </form>
 </body>
+
 </html>
---
+++ assets/components/forgotPass/newForgot.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="../register/confirmRegister.css">
+   <title>Document</title>
+</head>
+
+<body>
+   <div class="cont-confirm">
+      <form action="" method="POST">
+         <div class="bg-confirm">
+            <div class="confirm-text" style="margin-top: 50px;">
+               <label for="" class="lbl-pass-center" style="font-size: 25px;">Change Password</label>
+               <input type="password" class="input pass confirmCode" name="email" placeholder="New Password" required>
+               <input type="password" class="input pass confirmCode" name="email" placeholder="Confirm password" required>
+               <button type="submit" style="margin-top: 50px;" class="btn signin btn-confirmCode" name="submit">DONE</button>
+
+            </div>
+         </div>
+   </div>
+   </form>
+</body>
+
+</html>
