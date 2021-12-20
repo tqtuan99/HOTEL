369f6fdde7cc4af52c1a12407cc1762762e6443c
long40129 <nguyendailong2k@gmail.com>
2021-12-20 13:09:54 +0700

    update forgot pass

--- assets/components/forgotPass/forgotPass.php
+++ assets/components/forgotPass/forgotPass.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 require_once("../handle/dbcontroller.php");
 include('../handle/configDB.php');
 $check = 0;
--- assets/components/forgotPass/newForgot.php
+++ assets/components/forgotPass/newForgot.php
@@ -1,14 +1,26 @@
 <?php
 session_start();
+$check = 0;
 
-if(isset($_GET['q'])){
-   if(isset($_SESSION['tokenEmail'])){
-      
-   }
-   else 
-   {
+if (isset($_GET['q']) && isset($_SESSION['tokenEmail'])) {
+   if ($_SESSION['tokenEmail'] == $_GET['q']) {
+      require_once("../handle/dbcontroller.php");
+      $db_handle = new DBController();
+      if (isset($_POST['submit'])) {
+         $pass = $_POST['password'];
+         $Confirmpass = $_POST['confirmPassword'];
+         if ($pass == $Confirmpass) {
+            $querry = "UPDATE taikhoan SET matkhau = '$newPassMD5'";
+            $db_handle->updateQuery($querry);
+            unset($_SESSION['tokenEmail']);
+            $check = 1;
+         }
+      }
+   } else {
       header('Location: ../../../notFound.php');
    }
+} else {
+   header('Location: ../../../notFound.php');
 }
 ?>
 <!DOCTYPE html>
@@ -28,10 +40,13 @@ if(isset($_GET['q'])){
          <div class="bg-confirm">
             <div class="confirm-text" style="margin-top: 50px;">
                <label for="" class="lbl-pass-center" style="font-size: 25px;">Change Password</label>
-               <input type="password" class="input pass confirmCode" name="email" placeholder="New Password" required>
-               <input type="password" class="input pass confirmCode" name="email" placeholder="Confirm password" required>
+               <input type="password" class="input pass confirmCode" name="password" placeholder="New Password" required>
+               <input type="password" class="input pass confirmCode" name="confirmPassword" placeholder="Confirm password" required>
                <button type="submit" style="margin-top: 50px;" class="btn signin btn-confirmCode" name="submit">DONE</button>
-
+               <div style="font-size: 15px;margin: 28px; font-weight: 100;color: red;">
+                  <?php if (isset($_POST["submit"])) if ($check == 0) echo "Mã xác nhận không đúng!";
+                  else echo "Mật khẩu thay đổi thành công. <br><br><a href='../login/login.php?email=$email'>Quay lại trang đăng nhập</a>"; ?>
+               </div>
             </div>
          </div>
    </div>
